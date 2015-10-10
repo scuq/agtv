817a7b7bfff13bb7879c8e62f2acf35947eebc0c
scuq <sq+github@abyle.org>
2015-10-10 09:46:42 -0400

    git mess

--- twitchclientid.h
+++
@@ -1,8 +0,0 @@
-#ifndef TWITCHCLIENTID_H
-#define TWITCHCLIENTID_H
-
-//static QString twitchclientid="n2ea41n7vqlxfh3hs6zhiexuolyruyn";
-static QString twitchclientid="pb2gmicczhmqn5sb9vtfltph95zs4ji";
-
-#endif // TWITCHCLIENTID_H
-
