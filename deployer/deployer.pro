9e835105e419db5b99dd7006946c759069367970
scuq <sq+github@abyle.org>
2016-01-05 22:25:07 +0100

    added a few bits of the deployer subproject

---
+++ deployer/deployer.pro
@@ -0,0 +1,10 @@
+QT += core
+QT -= gui
+
+TARGET = deployer
+CONFIG += console
+CONFIG -= app_bundle
+
+TEMPLATE = app
+
+SOURCES += main.cpp
---
+++ deployer/main.cpp
@@ -0,0 +1,142 @@
+#include <QCoreApplication>
+#include <QCommandLineParser>
+#include <QDateTime>
+#include <QTextStream>
+#include <QSysInfo>
+#include <QTimer>
+
+QString getPlatform() {
+#ifdef Q_OS_WIN32
+    return QString("win");
+#endif
+#ifdef Q_OS_LINUX
+    return "lin";
+#endif
+#ifdef Q_OS_MAC
+    return "osx";
+#endif
+}
+
+QString getArch() {
+    return QSysInfo::currentCpuArchitecture();
+}
+
+QString getVersion(QString level) {
+
+    bool lvlfound = false;
+    QString versionstr = "";
+
+
+    if (level == "major") {
+       versionstr = QDateTime::currentDateTime().toString("yy");
+       lvlfound=true;
+    }
+
+    if ((level == "minor") && (lvlfound == false)) {
+       versionstr = QDateTime::currentDateTime().toString("MM");
+       lvlfound=true;
+    }
+
+    if ((level == "patch") && (lvlfound == false)) {
+       versionstr = QDateTime::currentDateTime().toString("dd");
+       lvlfound=true;
+    }
+
+    if ((level == "build") && (lvlfound == false)) {
+       versionstr = QDateTime::currentDateTime().toString("HHmm");
+       lvlfound=true;
+    }
+
+
+
+
+    return versionstr;
+}
+
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+
+    QCommandLineParser parser;
+        parser.setApplicationDescription("deployer");
+        parser.addHelpOption();
+        parser.addVersionOption();
+
+
+    QCommandLineOption versionMajor(QStringList() << "m" << "major",
+               QCoreApplication::translate("main", "print major version"));
+
+    QCommandLineOption versionMinor(QStringList() << "i" << "minor",
+               QCoreApplication::translate("main", "print minor version"));
+
+    QCommandLineOption versionPatch(QStringList() << "p" << "patch",
+               QCoreApplication::translate("main", "print patch version"));
+
+    QCommandLineOption versionBuild(QStringList() << "b" << "build",
+               QCoreApplication::translate("main", "print build version"));
+
+    QCommandLineOption platform(QStringList() << "o" << "platform",
+               QCoreApplication::translate("main", "print platform"));
+
+    QCommandLineOption arch(QStringList() << "a" << "arch",
+               QCoreApplication::translate("main", "print architecture"));
+
+    parser.addOption(versionMajor);
+    parser.addOption(versionMinor);
+    parser.addOption(versionPatch);
+    parser.addOption(versionBuild);
+    parser.addOption(platform);
+    parser.addOption(arch);
+
+    parser.process(a);
+
+    bool skip = false;
+
+
+    if ((parser.isSet(versionMajor) == true ) && (skip == false)) {
+         QTextStream(stdout) << getVersion("major") << "\n";
+         skip = true;
+    }
+
+    if ((parser.isSet(versionMinor) == true ) && (skip == false)) {
+         QTextStream(stdout) << getVersion("minor") << "\n";
+         skip = true;
+    }
+
+    if ((parser.isSet(versionPatch) == true ) && (skip == false)) {
+         QTextStream(stdout) << getVersion("patch") << "\n";
+         skip = true;
+    }
+
+    if ((parser.isSet(versionBuild) == true ) && (skip == false)) {
+         QTextStream(stdout) << getVersion("build") << "\n";
+         skip = true;
+    }
+
+    if ((parser.isSet(platform) == true ) && (skip == false)) {
+         QTextStream(stdout) << getPlatform() << "\n";
+         skip = true;
+    }
+
+    if ((parser.isSet(arch) == true ) && (skip == false)) {
+         QTextStream(stdout) << getArch() << "\n";
+         skip = true;
+    }
+
+
+
+    if (skip == false) {
+        QTextStream(stdout) << getVersion("major")+"."+getVersion("minor")+"."+getVersion("patch")+"."+getVersion("build");
+    }
+
+
+    QTimer::singleShot(200, &a, SLOT(quit()));
+    return a.exec();
+}
+
