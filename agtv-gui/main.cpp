e13db233b3a873c96e42122db92e57374de2193e
Hans-Peter Schadler <hps@abyle.org>
2016-01-08 14:48:50 +0100

    Removed instance check
    In Linux and OS X the memory area does not get cleaned if the programm
    crashes and it is not possible anymore to start it again without
    restarting.

--- agtv-gui/main.cpp
+++ agtv-gui/main.cpp
@@ -7,20 +7,16 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    QSharedMemory mem("CvZcgbqvJ9XIKqx1bZE7");
-     if(!mem.create(1))
-     {
-        QMessageBox::critical(0,"Instance detected!",genericHelper::getAppName()+" is already running!\nApplication terminating...","Ok");
-        exit(0);
-     }
-
-        tpMainWindow w;
-        w.show();
-
-
-
-        return a.exec();
-
-
+    
+//    QSharedMemory mem("CvZcgbqvJ9XIKqx1bZE7");
+//    if(!mem.create(1))
+//    {
+//        QMessageBox::critical(0,"Instance detected!",genericHelper::getAppName()+" is already running!\nApplication terminating...","Ok");
+//        exit(0);
+//    }
+    
+    tpMainWindow w;
+    w.show();
+    
+    return a.exec();
 }
